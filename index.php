7ddac4f1741f0d500f37c2b2cc6af1158446ac2d
roche <haoshichen@outlook.fr>
2018-03-22 09:29:50 +0100

    modif2

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <head>
 	<title>API Example</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript">
     var accessToken ="578c10fee57d4b3da72cc3bf683a41ce";
